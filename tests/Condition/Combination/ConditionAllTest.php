907f2806f1c04dc114b2234e1ed599585bd41c58
pascalheidmann <inf100291@fh-wedel.de>
2021-11-12 23:59:17 +0100

    add phpunit tests

---
+++ tests/Condition/Combination/ConditionAllTest.php
@@ -0,0 +1,62 @@
+<?php
+declare(strict_types=1);
+
+namespace PascalHeidmann\FeatureToggle\Tests\Condition\Combination;
+
+use PascalHeidmann\FeatureToggle\Condition\Combination\ConditionAll;
+use PascalHeidmann\FeatureToggle\Condition\StaticCondition;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \PascalHeidmann\FeatureToggle\Condition\Combination\ConditionAll
+ */
+final class ConditionAllTest extends TestCase
+{
+	/**
+	 * @test
+	 * @dataProvider conditions
+	 */
+	public function itWillEvaluate(bool $expected, ConditionAll $condition): void
+	{
+		self::assertEquals($expected, $condition->evaluate());
+	}
+
+	/**
+	 * @return iterable
+	 */
+	public function conditions(): iterable
+	{
+		$positive = new StaticCondition(true);
+		$negative = new StaticCondition(false);
+
+		yield 'none' => [
+			true,
+			new ConditionAll(),
+		];
+
+		yield 'positive' => [
+			true,
+			new ConditionAll($positive),
+		];
+
+		yield 'negative' => [
+			false,
+			new ConditionAll($negative),
+		];
+
+		yield 'negative + positive #1' => [
+			false,
+			new ConditionAll($positive, $negative),
+		];
+
+		yield 'negative + positive #2' => [
+			false,
+			new ConditionAll($negative, $positive),
+		];
+
+		yield 'negative + positive #3' => [
+			false,
+			new ConditionAll($negative, $negative, $positive),
+		];
+	}
+}
---
+++ tests/Condition/Combination/ConditionAnyTest.php
@@ -0,0 +1,62 @@
+<?php
+declare(strict_types=1);
+
+namespace PascalHeidmann\FeatureToggle\Tests\Condition\Combination;
+
+use PascalHeidmann\FeatureToggle\Condition\Combination\ConditionAny;
+use PascalHeidmann\FeatureToggle\Condition\StaticCondition;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \PascalHeidmann\FeatureToggle\Condition\Combination\ConditionAny
+ */
+final class ConditionAnyTest extends TestCase
+{
+	/**
+	 * @test
+	 * @dataProvider conditions
+	 */
+	public function itWillEvaluate(bool $expected, ConditionAny $condition): void
+	{
+		self::assertEquals($expected, $condition->evaluate());
+	}
+
+	/**
+	 * @return iterable
+	 */
+	public function conditions(): iterable
+	{
+		$positive = new StaticCondition(true);
+		$negative = new StaticCondition(false);
+
+		yield 'none' => [
+			false,
+			new ConditionAny(),
+		];
+
+		yield 'positive' => [
+			true,
+			new ConditionAny($positive),
+		];
+
+		yield 'negative' => [
+			false,
+			new ConditionAny($negative),
+		];
+
+		yield 'negative + positive #1' => [
+			true,
+			new ConditionAny($positive, $negative),
+		];
+
+		yield 'negative + positive #2' => [
+			true,
+			new ConditionAny($negative, $positive),
+		];
+
+		yield 'negative + positive #3' => [
+			true,
+			new ConditionAny($negative, $negative, $positive),
+		];
+	}
+}
