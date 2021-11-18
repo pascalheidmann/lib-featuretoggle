57dae94fe20d03fc615225504249d71da8156c52
pascalheidmann <inf100291@fh-wedel.de>
2021-11-18 23:26:22 +0100

    new CallbackCondition
    Signed-off-by: pascalheidmann <inf100291@fh-wedel.de>

---
+++ src/Condition/CallbackCondition.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace PascalHeidmann\FeatureToggle\Condition;
+
+class CallbackCondition implements ConditionInterface
+{
+	/** @var callable(array<string, mixed> $data): bool */
+	private $callback;
+
+	/**
+	 * @param callable(array<string, mixed> $data): bool $callback
+	 */
+	public function __construct(callable $callback)
+	{
+		$this->callback = $callback;
+	}
+
+	public function evaluate(array $data = []): bool
+	{
+		return ($this->callback)($data);
+	}
+}
