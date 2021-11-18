0f55dead39bcc442577ac33bd31922a39b97123b
pascalheidmann <inf100291@fh-wedel.de>
2021-11-18 23:25:15 +0100

    Let FeatureToggleManager define default values to be passed to feature toggles instead of requiring them to be passed
    Signed-off-by: pascalheidmann <inf100291@fh-wedel.de>

--- src/FeatureToggleManager.php
+++ src/FeatureToggleManager.php
@@ -10,10 +10,24 @@ class FeatureToggleManager
 {
 	/** @var RepositoryInterface[] */
 	private array $featureToggleRepository;
+	/** @var array<string, mixed> */
+	private array $defaultData = [];
 
 	public function __construct(RepositoryInterface ...$featureToggleRepository)
 	{
 		$this->featureToggleRepository = $featureToggleRepository;
+	}
+
+	/**
+	 * @param array<string, mixed> $data
+	 */
+	public function addDefaultData(array $data): void
+	{
+		$this->defaultData = array_merge($this->defaultData, $data);
+	}
+
+	public function resetDefaultData(): void {
+		$this->defaultData = [];
 	}
 
 	public function addRepository(RepositoryInterface $repository): self
@@ -31,7 +45,7 @@ class FeatureToggleManager
 			$featureToggle = $repository->get($key);
 
 			if ($featureToggle !== null) {
-				return $featureToggle->evaluate($data);
+				return $featureToggle->evaluate(array_merge($this->defaultData, $data));
 			}
 		}
 		throw new FeatureToggleNotFoundException($key);
